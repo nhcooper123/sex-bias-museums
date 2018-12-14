fed13466adce4c7562caef31975980493bea17cc
Natalie Cooper <ncooper@tcd.ie>
2018-12-14 11:10:32 +0000

    Delete code that is not needed

--- analyses/figure-years-all.R
+++ analyses/figure-years-all.R
@@ -20,21 +20,6 @@ ds_years <-
   mutate(percentf = (female/n) * 100) %>%
   distinct()
 
-#------------------------------------------------------------------
-# Fit models so we can add best fit lines
-
-ds_years_birds <- filter(ds_years, class == "Birds")
-ds_years_mammals <- filter(ds_years, class == "Mammals")
-year_model_birds <- glm(cbind(female, male) ~ year, data = ds_years_birds, family = "binomial")
-year_model_mammals <- glm(cbind(female, male) ~ year, data = ds_years_mammals, family = "binomial")
-
-newX <- data.frame(year = seq(from = 1880, to = 2010, length = 100))
-newYb <- predict.glm(year_model_birds, type = "response", newdata = newX, se.fit = TRUE)
-addTheseb <- data.frame(newX, newYb, class = rep("Birds", 100))
-
-newYm <- predict.glm(year_model_mammals, type = "response", newdata = newX, se.fit = TRUE)
-addThesem <- data.frame(newX, newYm)
-
 #-------------------------------------------------------------------
 # Plot % female by year
 plot_year <-   
