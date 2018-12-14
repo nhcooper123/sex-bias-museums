fbbc4cb104c955da6178dcac7d37944388d3c5ed
Natalie Cooper <ncooper@tcd.ie>
2018-12-14 11:51:20 +0000

    Initial attempts at SSD code

--- analyses/figure-ssd-all.R
+++ analyses/figure-ssd-all.R
@@ -1,11 +1,12 @@
 # Figures for SSD
 # Natalie Cooper 
-# July/Aug 2018
+# Dec 2018
 #------------------------
 # Load libraries
 library(tidyverse)
 library(viridis)
 library(patchwork)
+library(here)
 
 # Add rphylopic
 #install.packages("remotes")
@@ -14,12 +15,18 @@ library(rphylopic)
 
 # Read in phylopics
 library(png)
-img_tit <- readPNG("birds/img/tit.png")
-img_parrot <- readPNG("birds/img/parrot.png")
-img_shorebird <- readPNG("birds/img/shorebird.png")
-img_pigeon <- readPNG("birds/img/pigeon.png")
-img_hummingbird <- readPNG("birds/img/hummingbird.png")
-img_woodpecker <- readPNG("birds/img/woodpecker.png")
+img_tit <- readPNG(here("img/tit.png"))
+img_parrot <- readPNG(here("img/parrot.png"))
+img_shorebird <- readPNG(here("img/shorebird.png"))
+img_pigeon <- readPNG(here("img/pigeon.png"))
+img_hummingbird <- readPNG(here("img/hummingbird.png"))
+img_woodpecker <- readPNG(here("img/woodpecker.png"))
+img_bat <- readPNG(here("img/bat.png"))
+img_mouse <- readPNG(here("img/mouse.png"))
+img_tiger <- readPNG(here("img/tiger.png"))
+img_deer <- readPNG(here("img/deer.png"))
+img_shrew <- readPNG(here("img/shrew.png"))
+img_monkey <- readPNG(here("img/monkey.png"))
 
 # Colour blind friendly palette
 # First is grey
@@ -38,95 +45,91 @@ remove_x <-
         axis.ticks.x=element_blank())
 #-------------------------------------------------------------
 # Read in all the data
-specimens <- read_csv("birds/data/all-specimen-data-birds.csv")
-glimpse(specimens)
+specimens <- read_csv(here("data/all-specimen-data.csv"))
 
-# Summarise by species
-# Extract numbers of males and females
-# proportions, and totals
-# Remove species with < 100 specimens
+#------------------------
+# Extract data required
+#------------------------
 ds <-
   specimens %>%
-  group_by(binomial, order) %>%
-  summarise(f = sum(sex == "Female"),
-            m = sum(sex == "Male"),
-            total = n(),
-            propf = f/total*100,
-            propm = m/total*100) %>%
-  filter(total >= 100)
-
-# Check this 
-ds
+  filter(!is.na(sex)) %>%
+  group_by(class) %>%
+  add_count(binomial) %>%
+  add_count(binomial, sex) %>%
+  filter(n >= 100 & sex == "Female") %>%
+  mutate(percent = nn/n*100) %>%
+  select(class, order, binomial, percent) %>%
+  distinct()
 
 #-------------------------------------------------------------
-# Read in the body size data
-extra <- read_csv("birds/data/all-extra-data-birds.csv")
-glimpse(extra)
+# Read in the body size data and merge
+#-------------------------------------------------------------
+extra <- read_csv("data/all-extra-data.csv")
 
 # Merge with the specimen data
 # Exclude anything that doesn't have specimen data
-ds_SSD <- left_join(ds, extra)
+ds_ssd <- left_join(ds, extra)
+
 #-------------------------------------------------------------
 # Plotting
-# 1. All species
-ggplot(ds_SSD, aes(x = log(SSD), y = propf)) +
-  geom_point() +
-  geom_hline(yintercept = 50, linetype = 2) +
-  geom_vline(xintercept = 0, linetype = 3) +
+# All species together
+#-------------------------------------------------------------
+plot_ssd <-   
+ggplot(ds_ssd, aes(x = log(SSD), y = percent/100, colour = class)) +
+  geom_point(alpha = 0.8, size = 0.5) +
+  geom_smooth(method = "glm",
+              method.args = list(family = "binomial"), 
+              col = "darkgrey", size = 0.5) +
+ geom_abline(slope = 0, intercept = 0.5, linetype = 2) +
   theme_bw(base_size = 14) +
-  xlab("log sexual size dimorphism") +
   ylab("% female specimens") +
-  ylim(0, 100)
-
-ggsave("birds/figures/ssd-all-birds.png")
-#-------------------------------------------------------------------
-# 2. All orders
-
-ggplot(ds_SSD, aes(x = log(SSD), y = propf, colour = order)) +
-  geom_point() +
-  geom_hline(yintercept = 50, linetype = 2) +
-  geom_vline(xintercept = 0, linetype = 3) +
-  theme_bw(base_size = 14) +
-  xlab("log SSD") +
-  ylab("% female specimens") +
-  ylim(0, 100) +
+  xlab("Collection year") +
+  ylim(0, 1) +
+  scale_colour_manual(values = c(cbPalette[c(2:5)])) +
+  xlim(-1, 2) +
   theme(legend.position = "none",
         strip.background = element_rect(fill="white")) +
-  facet_wrap(~order) 
+  facet_wrap(~class, ncol = 2)
 
-ggsave("birds/figures/ssd-orders-all-birds.png")
+plot_ssd
+
+ggsave("figures/ssd-all.png")
 #-------------------------------------------------------------------
-# 3. Order plots (Top 6 only)
-
+# BIRDS
+# Order plots (Top 6 only)
+# Passeriformes, Apodiformes, Piciformes
+# Psittaciformes, Charadriiformes, Columbiformes
+#-------------------------------------------------------------------
 # Create new dataframes for each order
 ds_pass <- 
-  ds_SSD %>%
+  ds_ssd %>%
   filter(order == "Passeriformes")
 
 ds_hum <- 
-  ds_SSD %>%
+  ds_ssd %>%
   filter(order == "Apodiformes")
 
 ds_wood <- 
-  ds_SSD %>%
+  ds_ssd %>%
   filter(order == "Piciformes")
 
 ds_parrot <- 
-  ds_SSD %>%
+  ds_ssd %>%
   filter(order == "Psittaciformes")
 
 ds_shore <- 
-  ds_SSD %>%
+  ds_ssd %>%
   filter(order == "Charadriiformes")
 
 ds_pigeon <- 
-  ds_SSD %>%
+  ds_ssd %>%
   filter(order == "Columbiformes")
 
 #--------------------------------------
 # Make base plot
+#--------------------------------------
 base <-
-  ggplot(ds_SSD, aes(x = log(SSD), y = propf)) +
+  ggplot(ds_ssd, aes(x = log(SSD), y = percent)) +
   geom_hline(yintercept = 50, linetype = 2) +
   geom_vline(xintercept = 0, linetype = 3) +
   theme_bw(base_size = 14) +
@@ -135,7 +138,9 @@ base <-
   ylim(0, 100) +
   xlim(-0.5, 0.5)
 
+#--------------------------------------
 # Make plots for each order
+#--------------------------------------
 pass <-
   base +
   geom_point(data = ds_pass, colour = cbPalette[2]) +
@@ -154,12 +159,12 @@ wood <-
   geom_point(data = ds_wood, colour = cbPalette[4]) +
   add_phylopic(img = img_woodpecker, alpha = 1, x = -0.4, y = 95, ysize = 15) +
   remove_x +
-  remove_y
+  remove_y 
 
 parrot <-
   base +
   geom_point(data = ds_parrot, colour = cbPalette[5]) +
-  add_phylopic(img = img_parrot, alpha = 1, x = -0.4, y = 95, ysize = 15)
+  add_phylopic(img = img_parrot, alpha = 1, x = -0.4, y = 95, ysize = 15) 
 
 shore <-
   base +
@@ -176,4 +181,92 @@ pigeon <-
 
 (pass | hum | wood)/(parrot | shore | pigeon)
 
-ggsave("birds/figures/ssd-orders-six-birds.png")
+ggsave("figures/ssd-orders-birds.png", height = 100, units = "mm")
+#-------------------------------------------------------------------
+# MAMMALS
+# Order plots (Top 6 orders only)
+# Rodentia, Chiroptera, Soriciomorpha, 
+# Carnivora, Primates, Artiodactyla
+#-------------------------------------------------------------------
+# Create new dataframes for each order
+ds_rodent <- 
+  ds_ssd %>%
+  filter(order == "Rodentia")
+
+ds_bat <- 
+  ds_ssd %>%
+  filter(order == "Chiroptera")
+
+ds_sor <- 
+  ds_ssd %>%
+  filter(order == "Soricomorpha")
+
+ds_carn <- 
+  ds_ssd %>%
+  filter(order == "Carnivora")
+
+ds_primate <- 
+  ds_ssd %>%
+  filter(order == "Primates")
+
+ds_artio <- 
+  ds_ssd %>%
+  filter(order == "Artiodactyla")
+
+#--------------------------------------
+# Make base plot
+#--------------------------------------
+base <-
+  ggplot(ds_ssd, aes(x = log(SSD), y = percent)) +
+  geom_hline(yintercept = 50, linetype = 2) +
+  geom_vline(xintercept = 0, linetype = 3) +
+  theme_bw(base_size = 14) +
+  xlab("") +
+  ylab("% female specimens") +
+  ylim(0, 100) +
+  xlim(-1, 1.5)
+
+#--------------------------------------
+# Make plots for each order
+#--------------------------------------
+rodent <-
+  base +
+  geom_point(data = ds_rodent, colour = cbPalette[2]) +
+  add_phylopic(img = img_mouse, alpha = 1, x = -0.75, y = 93, ysize = 9) +
+  remove_x
+
+bat <-
+  base +
+  geom_point(data = ds_bat, colour = cbPalette[3]) +
+  add_phylopic(img = img_bat, alpha = 1, x = -0.75, y = 93, ysize = 11) +
+  remove_x +
+  remove_y
+
+sor <-
+  base +
+  geom_point(data = ds_sor, colour = cbPalette[4]) +
+  add_phylopic(img = img_shrew, alpha = 1, x = -0.75, y = 93, ysize = 8) +
+  remove_x +
+  remove_y
+
+carn <-
+  base +
+  geom_point(data = ds_carn, colour = cbPalette[5]) +
+  add_phylopic(img = img_tiger, alpha = 1, x = -0.75, y = 93, ysize = 7)
+
+primate <-
+  base +
+  geom_point(data = ds_primate, colour = cbPalette[8]) +
+  add_phylopic(img = img_monkey, alpha = 1, x = -0.70, y = 93, ysize = 11) +
+  remove_y +
+  xlab("log sexual size dimorphism")
+
+artio <-
+  base +
+  geom_point(data = ds_artio, colour = cbPalette[7]) +
+  add_phylopic(img = img_deer, alpha = 1, x = -0.75, y = 93, ysize = 16) +
+  remove_y
+
+(rodent | bat | sor)/(carn|primate|artio)
+
+ggsave("figures/ssd-orders-mammals.png", height = 100, units = "mm")
