a5caac402410240293c6340c1e2fc26d5f505224
Natalie Cooper <ncooper@tcd.ie>
2019-02-11 14:15:42 +0000

    Convert to % on y axis

--- analyses/figure-years-all.R
+++ analyses/figure-years-all.R
@@ -1,6 +1,6 @@
 # Figure for years
 # Natalie Cooper
-# Dec 2018
+# Feb 2019
 #------------------------------------
 # Summarise by species by year
 # Extract numbers of males and females
@@ -26,18 +26,19 @@ plot_year <-
 ggplot(ds_years, aes(x = year, y = percentf/100, colour = class)) +
   geom_point(alpha = 0.8, size = 0.5) +
   geom_smooth(method = "glm",
-              method.args = list(family = "binomial"), 
+              method.args = list(family = "quasibinomial"), 
               col = "darkgrey", size = 0.5) +
- geom_abline(slope = 0, intercept = 0.5, linetype = 2) +
+  geom_abline(slope = 0, intercept = 0.5, linetype = 2) +
   theme_bw(base_size = 14) +
   ylab("% female specimens") +
   xlab("Collection year") +
-  ylim(0, 1) +
+  #ylim(0, 1) +
   scale_colour_manual(values = c(cbPalette[c(2:5)])) +
+  scale_y_continuous(labels = c(0,25,50,75,100)) +
   xlim(1880, 2010) +
   theme(legend.position = "none",
         strip.background = element_rect(fill="white")) +
   facet_wrap(~class, ncol = 1)
 
 plot_year
-#ggsave("figures/years-all.png", width = 80, units = "mm")
+ggsave("figures/years-all.png", width = 80, units = "mm")
